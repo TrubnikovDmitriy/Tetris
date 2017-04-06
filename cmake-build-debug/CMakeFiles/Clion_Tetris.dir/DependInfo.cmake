7b6a25ba67a730223f40d0528f9b83e7f7320048
Dmitriy Trubnikov <trubnikovdv@mail.ru>
2017-04-06 22:54:15 +0100

    images, sounds, fonts, etc

---
+++ cmake-build-debug/CMakeFiles/Clion_Tetris.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/dmitriy/HomeWork/Tetris/Clion-Tetris/Blocks.cpp" "/home/dmitriy/HomeWork/Tetris/Clion-Tetris/cmake-build-debug/CMakeFiles/Clion_Tetris.dir/Blocks.cpp.o"
+  "/home/dmitriy/HomeWork/Tetris/Clion-Tetris/Field.cpp" "/home/dmitriy/HomeWork/Tetris/Clion-Tetris/cmake-build-debug/CMakeFiles/Clion_Tetris.dir/Field.cpp.o"
+  "/home/dmitriy/HomeWork/Tetris/Clion-Tetris/Figure.cpp" "/home/dmitriy/HomeWork/Tetris/Clion-Tetris/cmake-build-debug/CMakeFiles/Clion_Tetris.dir/Figure.cpp.o"
+  "/home/dmitriy/HomeWork/Tetris/Clion-Tetris/main.cpp" "/home/dmitriy/HomeWork/Tetris/Clion-Tetris/cmake-build-debug/CMakeFiles/Clion_Tetris.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Clion_Tetris.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Clion_Tetris.dir/main.cpp.o"
+  "CMakeFiles/Clion_Tetris.dir/Field.cpp.o"
+  "CMakeFiles/Clion_Tetris.dir/Figure.cpp.o"
+  "CMakeFiles/Clion_Tetris.dir/Blocks.cpp.o"
+  "Clion_Tetris.pdb"
+  "Clion_Tetris"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Clion_Tetris.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
